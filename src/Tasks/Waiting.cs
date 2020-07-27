fba53f76054dc6feb3da6fea1c278cf083d969dd
Miguel <migueldreis01@gmail.com>
2020-07-27 20:33:25 -0300

    Waiting for tasks

--- src/Tasks/Waiting.cs
+++ src/Tasks/Waiting.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace parallel_programming.Tasks
 {
@@ -8,7 +10,22 @@ namespace parallel_programming.Tasks
     {
         public static void Main()
         {
-             
+            var cancellationSource = new CancellationTokenSource();
+            var token = cancellationSource.Token;
+
+            var task = new Task(() =>
+            {
+                Console.WriteLine("Você tem cinco segundos para apertar uma tecla");
+                bool cancelled = token.WaitHandle.WaitOne(5000);
+                Console.WriteLine(cancelled ? "Tecla apertada a tempo" : "Tecla não apertada a tempo");
+            }, token);
+
+            task.Start();
+
+            Console.ReadKey();
+            cancellationSource.Cancel();
+
+            Console.ReadKey();
         }
     }
 }
---
+++ src/Tasks/Waiting2.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace parallel_programming.Tasks
+{
+    public static class Waiting2
+    {
+        public static void WaitForOneTask()
+        {
+            var cts = new CancellationTokenSource();
+            var token = cts.Token;
+
+            var task1 = new Task(() =>
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    token.ThrowIfCancellationRequested();
+                    Thread.Sleep(1000);
+                }
+                Console.WriteLine("task 1 completa");
+            }, token);
+
+            task1.Start();
+            task1.Wait(token);
+
+            Console.WriteLine("Fim do método");
+            Console.ReadKey();
+        }
+
+        public static void WaitForAllTasks()
+        {
+            var cts = new CancellationTokenSource();
+            var token = cts.Token;
+
+            var task1 = new Task(() =>
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    token.ThrowIfCancellationRequested();
+                    Thread.Sleep(1000);
+                }
+                Console.WriteLine("task 1 completa");
+            }, token);
+
+            task1.Start();
+            var task2 = Task.Factory.StartNew(() => Thread.Sleep(3000), token);
+
+            Task.WaitAll(task1, task2);
+
+            Console.WriteLine("Fim do método");
+            Console.ReadKey();
+        }
+
+        public static void WaitForShortestTask()
+        {
+            var cts = new CancellationTokenSource();
+            var token = cts.Token;
+
+            var task1 = new Task(() =>
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    token.ThrowIfCancellationRequested();
+                    Thread.Sleep(1000);
+                }
+                Console.WriteLine("task 1 completa");
+            }, token);
+
+            task1.Start();
+            var task2 = Task.Factory.StartNew(() => Thread.Sleep(3000), token);
+
+            Task.WaitAny(task1, task2);
+
+            Console.WriteLine("Fim do método");
+            Console.ReadKey();
+        }
+
+        public static void WaitForShortestTaskWithTimeout()
+        {
+            var cts = new CancellationTokenSource();
+            var token = cts.Token;
+
+            var task1 = new Task(() =>
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    token.ThrowIfCancellationRequested();
+                    Thread.Sleep(1000);
+                }
+                Console.WriteLine("task 1 completa");
+            }, token);
+
+            task1.Start();
+            var task2 = Task.Factory.StartNew(() => Thread.Sleep(6000), token);
+
+            Task.WaitAny(new[] { task1, task2 }, 2000, token); 
+            Console.WriteLine("Fim do método");
+            //Imprime "Fim do Método" antes de "task 1 completa" pois para de esperar após 2s
+
+            Console.ReadKey();
+        }
+
+        public static void Main()
+        {
+            WaitForShortestTaskWithTimeout();
+        }
+    }
+}
